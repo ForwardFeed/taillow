7c35baeeff4bb7d241b5b9e532c427e78c79c384
ForwardFeed <76596109+ForwardFeed@users.noreply.github.com>
2024-09-12 18:05:30 +0200

    started to work on the UI

---
+++ ui/src/components/icons/WelcomeScreen.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+    
+</script>
+<template>
+    <div>
+        Welcome to the Elite Redux officially supported online toolbox
+    </div>
+</template>
+<style>
+    
+</style>
--- ui/src/views/HomeView.vue
+++ ui/src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import WelcomeScreen from '@/components/icons/WelcomeScreen.vue';
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <WelcomeScreen/>
   </main>
 </template>
