f29d96ea1d12d387de5eacabb46245da6047295e
ForwardFeed <76596109+ForwardFeed@users.noreply.github.com>
2024-10-25 23:08:39 +0200

    started to work on the design of the searchbar / filter / tags

---
+++ ui/src/data/search/search.ts
@@ -0,0 +1,49 @@
+
+const ROData = ["a", "b", "c"] as const
+type DATATYPE = typeof ROData
+export const data = structuredClone(ROData)
+
+const handler = {
+    get(target, prop, receiver ){
+        if (prop === "")
+            return 0
+    }
+}
+
+const filterOperators = ["AND", "OR"] as const
+type FilterOperators = (typeof filterOperators)[number]
+//historyOfFilters ?
+type SearchFilterGroup = {
+    filters: Array<SearchFilter | FilterOperators>
+}
+
+const availableFields = ["name", "type"] as const
+type AvailableFields = (typeof availableFields)[number]
+type SearchFilter = {
+    field: AvailableFields,
+    data: string,
+    inverted: boolean,
+    optionnal: boolean,
+    valid: boolean
+}
+const filtersData: SearchFilter[] = []
+function applyFilter(filters: SearchFilter[]){
+    const valids = filters.map(x => x.valid)
+    
+}
+
+type SearchReorder = {
+    field: string
+}
+
+function reorder(){
+
+}
+
+function createPreSearchTree(){
+
+}
+
+const cache = []
+
+const dataProxy = new Proxy(ROData, handler)
