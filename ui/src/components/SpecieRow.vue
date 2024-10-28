4e1f28d067847a206e878960df6ea77798c3a6db
ForwardFeed <76596109+ForwardFeed@users.noreply.github.com>
2024-10-28 20:35:23 +0100

    visual changes on the pokemon

--- ui/src/components/SpecieRow.vue
+++ ui/src/components/SpecieRow.vue
@@ -13,6 +13,7 @@ function openView(){
     console.log("hide")
 }
 const viewState = ref(false)
+const maxViewState = ref(0)
 
 /**
  * The idea is that eggmoves can a number which is a specie ID that has the eggmoves in question
@@ -40,31 +41,45 @@ const eggMoves = getEggmoves(props.specie.mEggMoves)
         </div>
     </div>
     <div class="maximum-view" v-if="viewState">
-        <div class="move-list">
-            <div class="move-col" v-if="specie.mLevel.length">
-                <div class="move-col-title">learnset</div>
-                <div class="move-row" v-for="moveID of specie.mLevel" :key="moveID.id">
-                    {{ gamedata.moves[moveID.id].name}} at {{ moveID.lvl }}
+        <div class="main-content">
+            <div class="move-list content-block" v-if="maxViewState == 0">
+                <div class="move-col" v-if="specie.mLevel.length">
+                    <div class="move-col-title"><span>Learnset</span></div>
+                    <div class="move-row" v-for="{id, lvl} of specie.mLevel" :key="id">
+                        <span> {{ gamedata.moves[id].name }}</span>
+                        <span class="move-level"> {{ lvl }} </span>
+                    </div>
+                </div>
+                <div class="move-col" v-if="specie.mTMHM.length">
+                    <div class="move-col-title"><span>TMHM</span></div>
+                    <div class="move-row" v-for="moveID of specie.mTMHM" :key="moveID">
+                        {{ gamedata.moves[moveID].name}}
+                    </div>
+                </div>
+                <div class="move-col" v-if="specie.mTutors.length">
+                    <div class="move-col-title"><span>Tutor</span></div>
+                    <div class="move-row" v-for="moveID of specie.mTutors" :key="moveID">
+                        {{ gamedata.moves[moveID].name}}
+                    </div>
+                </div>
+                <div class="move-col" v-if="eggMoves.length">
+                    <div class="move-col-title"><span>Eggmoves</span></div>
+                    <div class="move-row" v-for="moveID of eggMoves" :key="moveID">
+                        {{ gamedata.moves[moveID].name}}
+                    </div>
                 </div>
             </div>
-            <div class="move-col" v-if="specie.mTMHM.length">
-                <div class="move-col-title">TMHM</div>
-                <div class="move-row" v-for="moveID of specie.mTMHM" :key="moveID">
-                    {{ gamedata.moves[moveID].name}}
-                </div>
+            <div class="content-block" v-else-if="maxViewState == 1">
+                {{ specie.desc }}
             </div>
-            <div class="move-col" v-if="specie.mTutors.length">
-                <div class="move-col-title">Tutor</div>
-                <div class="move-row" v-for="moveID of specie.mTutors" :key="moveID">
-                    {{ gamedata.moves[moveID].name}}
-                </div>
+            <div class="content-block" v-else-if="maxViewState == 2">
+                Sets
             </div>
-            <div class="move-col" v-if="eggMoves.length">
-                <div class="move-col-title">Eggmoves</div>
-                <div class="move-row" v-for="moveID of eggMoves" :key="moveID">
-                    {{ gamedata.moves[moveID].name}}
-                </div>
-            </div>
+        </div>
+        <div class="sidebar">
+            <button @click="maxViewState = 0">Moves</button>
+            <button @click="maxViewState = 1">Misc</button>
+            <button @click="maxViewState = 2">Sets</button>
         </div>
     </div>
 </div>
@@ -77,11 +92,49 @@ const eggMoves = getEggmoves(props.specie.mEggMoves)
         width: 100%;
         display: flex;
     }
+    .minimun-view{
+        background-color: rgb(113, 31, 221);
+    }
     .maximum-view{
         background-color: beige;
         z-index: 0;
+        height: 30em;
+    }
+    .sidebar{
+        display: flex;
+        flex-direction: column;
+        height: 100%;
+        width: 4em;
+        background-color: rgb(70, 150, 88);
+    }
+    .sidebar > button{
+        margin: auto;
+    }
+    .main-content{
+        flex-grow: 1;
+    }
+    .content-block{
+        width: 100%;
+        background-color: deepskyblue;
+    }
+    .move-col{
+        flex-grow: 1;
+        display: flex;
+        flex-wrap: wrap;
+    }
+    .move-col-title{
+        display: flex;
+        width: 100%;
     }
     .move-list{
         display: flex;
     }
+    .move-row{
+        width: 12em;
+        display: flex;
+    }
+    .move-level{
+        margin-right: 0;
+    }
+   
 </style>
